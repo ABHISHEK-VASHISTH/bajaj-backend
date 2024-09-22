45329f66e32084ddeb66132bd15c531d9e0d24df
Abhishek Vashisth <ap9359@srmist.edu.in>
2024-09-22 12:17:45 +0000

    Update to Netlify function

--- functions/bfhl.js
+++ functions/bfhl.js
@@ -1,56 +1,54 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+exports.handler = async (event, context) => {
+  if (event.httpMethod === 'POST') {
+    const { data, file_b64 } = JSON.parse(event.body);
+    let numbers = [];
+    let alphabets = [];
+    let highestLowercaseAlphabet = null;
 
-app.use(bodyParser.json());
+    data.forEach((item) => {
+      if (!isNaN(item)) numbers.push(item);
+      else if (/^[a-zA-Z]+$/.test(item)) alphabets.push(item);
+    });
 
-app.post('/bfhl', (req, res) => {
-  const { data, file_b64 } = req.body;
-  let numbers = [];
-  let alphabets = [];
-  let highestLowercaseAlphabet = null;
+    const lowercaseAlphabets = alphabets.filter((char) => /[a-z]/.test(char));
+    if (lowercaseAlphabets.length > 0) {
+      highestLowercaseAlphabet = [lowercaseAlphabets.sort().pop()];
+    }
 
-  data.forEach((item) => {
-    if (!isNaN(item)) numbers.push(item);
-    else if (/^[a-zA-Z]+$/.test(item)) alphabets.push(item);
-  });
+    let fileValid = false;
+    let fileMimeType = null;
+    let fileSizeKB = 0;
 
+    if (file_b64) {
+      fileValid = true;
+      fileMimeType = "application/pdf"; 
+      fileSizeKB = (Buffer.byteLength(file_b64, 'base64') / 1024).toFixed(2);
+    }
 
-  const lowercaseAlphabets = alphabets.filter((char) => /[a-z]/.test(char));
-  if (lowercaseAlphabets.length > 0) {
-    highestLowercaseAlphabet = [lowercaseAlphabets.sort().pop()];
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        is_success: true,
+        user_id: "john_doe_17091999",
+        email: "john@xyz.com",
+        roll_number: "ABCD123",
+        numbers: numbers,
+        alphabets: alphabets,
+        highest_lowercase_alphabet: highestLowercaseAlphabet,
+        file_valid: fileValid,
+        file_mime_type: fileMimeType,
+        file_size_kb: fileSizeKB,
+      }),
+    };
+  } else if (event.httpMethod === 'GET') {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ operation_code: 1 }),
+    };
+  } else {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ message: 'Method Not Allowed' }),
+    };
   }
-
-  
-  let fileValid = false;
-  let fileMimeType = null;
-  let fileSizeKB = 0;
-
-  if (file_b64) {
-    fileValid = true;
-    fileMimeType = "application/pdf"; 
-    fileSizeKB = (Buffer.byteLength(file_b64, 'base64') / 1024).toFixed(2);
-  }
-
-  res.json({
-    "is_success": true,
-    "user_id": "john_doe_17091999",
-    "email": "john@xyz.com",
-    "roll_number": "ABCD123",
-    "numbers": numbers,
-    "alphabets": alphabets,
-    "highest_lowercase_alphabet": highestLowercaseAlphabet,
-    "file_valid": fileValid,
-    "file_mime_type": fileMimeType,
-    "file_size_kb": fileSizeKB
-  });
-});
-
-app.get('/bfhl', (req, res) => {
-  res.json({ "operation_code": 1 });
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+};
